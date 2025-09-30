b096283c5f65d9587f0d3a66798cddcfa7f86f40
Yitzhak Keidar <keidar17@gmail.com>
2025-09-30 18:25:13 +0300

    feat(game-logic): add MAX_DMG constant and random damage helper

--- src/modules/game.model.js
+++ src/modules/game.model.js
@@ -2,6 +2,12 @@ let enemyHp = 100;
 let userHp = 100;
 let isUserTurn = true;
 let isGameRunning = true;
+const MAX_DMG = 20;
+
+const generateRandomDmg = () => {
+  return Math.floor(Math.random() * MAX_DMG);
+}
+
 
 export const attack = () => {
   if (isUserTurn && isGameRunning) {
@@ -11,11 +17,21 @@ export const attack = () => {
 }
 
 const reduceEnemyHp = () => {
-  enemyHp -= 7;
+  enemyHp -= generateRandomDmg();
   console.log(`Enemy HP: ${enemyHp}`);
   switchTurns();
+  setEnenmyTimeAttack();
 }
 
+const setEnenmyTimeAttack = () => {
+  setTimeout(() => {
+    if (!isUserTurn && isGameRunning) {
+      userHp -= generateRandomDmg();
+      console.log(`User HP: ${userHp}`);
+      switchTurns();
+    }
+  }, 2000);
+}
 const switchTurns = () => {
   isUserTurn = !isUserTurn;
 } 
